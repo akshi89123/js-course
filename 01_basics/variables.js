1c8e30837d1ee53b5e02c03a4f4919cebe92d46f
Akshita Jha <2906akshita@gmail.com>
2026-02-14 13:57:54 +0000

    config

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const age = 91;
+ n = 'akshita';
+let course = 'btech';
+console.log(n);
+console.log(age);
+console.log(course);
+console.table([age , n ,course]);
+
+n = 'shreya';
+course = 'btech cse';
+
+console.table([age , n ,course]);
