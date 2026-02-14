09e804bb43a619213830052ffe613b704c3d7dd2
Akshita Jha <2906akshita@gmail.com>
2026-02-14 16:17:46 +0000

    operations

--- 01_basics/datatypecon.js
+++ 01_basics/datatypecon.js
@@ -4,13 +4,22 @@
 // let age = true;
 let age = "";
 
-console.log(typeof age);
+// console.log(typeof age);
 let valueInNumber = Number(age);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let isLoggedIn = 1;
 
 let valueIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof valueIsLoggedIn);
-console.log(valueIsLoggedIn);
+// console.log(typeof valueIsLoggedIn);
+// console.log(valueIsLoggedIn);
+
+let value = 234
+let negValue = -value;
+
+// console.log(negValue);
+console.log(++value);  //235
+console.log(value);    //235
+console.log(value++ ); //235   it prints value first then add, later it becomes 236
+console.log(value);    //236
