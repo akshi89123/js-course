31a6b756f8357925f3aebb55d9fa38c5027fe617
Akshita Jha <2906akshita@gmail.com>
2026-02-14 14:50:08 +0000

    datatype conversion

---
+++ 01_basics/datatypecon.js
@@ -0,0 +1,16 @@
+// let age = "";
+// let age = null;
+// let age = undefined;
+// let age = true;
+let age = "";
+
+console.log(typeof age);
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let valueIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof valueIsLoggedIn);
+console.log(valueIsLoggedIn);
